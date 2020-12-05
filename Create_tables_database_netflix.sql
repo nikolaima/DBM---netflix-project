d27767bbf62e9aeacf4762fa375b62dbb2411a2f
nikolaima <nikolai.maag@gmail.com>
2020-12-05 16:37:49 +0900

    Update Create_tables_database_netflix.sql
    added last_update and changed a few data types

--- Create_tables_database_netflix.sql
+++ Create_tables_database_netflix.sql
@@ -2,100 +2,119 @@ CREATE database dbm_netflix_project;
 USE dbm_netflix_project;
 
 CREATE table shows(
-show_id int primary key,
-type ENUM('Movie','TV Show'),
-title varchar(200),
+show_id int primary key NOT NULL,
+type ENUM('Movie','TV Show') NOT NULL,
+title varchar(200) NOT NULL,
 date_added datetime,
 release_year int,
 rating_code varchar(15),
 duration int,
 season_count int,
-description varchar (500)
+description varchar (500),
+last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 #table genre with 2 columns genre_id + genre + last_update
 CREATE TABLE genre(
-genre_id int primary key,
-genre varchar(100),
-last_update datetime default now()
+genre_id smallint primary key NOT NULL,
+genre varchar(100) NOT NULL,
+last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 #table actor with 3 columns actor_id + actor + last_update
 CREATE TABLE actor(
-actor_id int primary key,
-actor varchar(100),
-last_update datetime default now()
+actor_id int primary key NOT NULL,
+actor varchar(100) NOT NULL,
+last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 #table director with 3 columns director_id, director + last_update
 CREATE TABLE director(
-director_id int primary key,
-director varchar(100),
-last_update datetime default now()
+director_id int primary key NOT NULL,
+director varchar(100) NOT NULL,
+last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 #table rating with 3 columns rating_id, rating_description + las_update
 CREATE TABLE rating(
-rating_code varchar(100),
-rating_description varchar(200),
-last_update datetime default now()
+rating_code varchar(100) primary key NOT NULL,
+rating_description varchar(200) NOT NULL,
+last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 #table for country w countryId, country + last_update
 CREATE table country(
-country_id int primary key,
-country varchar(100),
-last_update datetime default now()
+country_id smallint primary key NOT NULL,
+country varchar(100) NOT NULL,
+last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 #table to connect n:m relationship between shows
 CREATE table show_country(
-show_id int,
-country_id int,
-last_update datetime default now(),
-FOREIGN KEY (show_id) REFERENCES shows(show_id),
-FOREIGN KEY (country_id) REFERENCES country(country_id)
+show_id int NOT NULL,
+country_id smallint NOT NULL,
+last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (show_id,country_id),
+CONSTRAINT `fk_show_country_show` FOREIGN KEY (show_id) REFERENCES shows(show_id),
+CONSTRAINT `fk_show_country_country` FOREIGN KEY (country_id) REFERENCES country(country_id)
 );
 
 CREATE table show_director(
-show_id int,
-director_id int,
-last_update datetime default now(),
-FOREIGN KEY (show_id) REFERENCES shows(show_id),
-FOREIGN KEY (director_id) REFERENCES director(director_id)
+show_id int NOT NULL,
+director_id int NOT NULL,
+last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (show_id, director_id),
+CONSTRAINT `fk_show_director_show` FOREIGN KEY (show_id) REFERENCES shows(show_id),
+CONSTRAINT `fk_show_director_director` FOREIGN KEY (director_id) REFERENCES director(director_id)
 );
 
 CREATE table show_actor(
-show_id int,
-actor_id int,
-last_update datetime default now(),
-FOREIGN KEY (show_id) REFERENCES shows(show_id),
-FOREIGN KEY (actor_id) REFERENCES actor(actor_id)
+show_id int NOT NULL,
+actor_id int NOT NULL,
+last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (show_id, actor_id),
+CONSTRAINT `fk_show_actor_show` FOREIGN KEY (show_id) REFERENCES shows(show_id),
+CONSTRAINT `fk_show_actor_actor` FOREIGN KEY (actor_id) REFERENCES actor(actor_id)
 );
 
 CREATE table show_genre(
-show_id int,
-genre_id int,
-last_update datetime default now(),
-FOREIGN KEY (show_id) REFERENCES shows(show_id),
-FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+show_id int NOT NULL,
+genre_id smallint NOT NULL,
+last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY(show_id, genre_id),
+CONSTRAINT `fk_show_genre_show` FOREIGN KEY (show_id) REFERENCES shows(show_id),
+CONSTRAINT `fk_show_genre_genre` FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+);
+
+CREATE TABLE users (
+  user_name varchar(100) NOT NULL PRIMARY KEY,
+  password varchar(100) NOT NULL,
+  last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE table users_shows(
+	user_name varchar(100) NOT NULL,
+    show_id int NOT NULL,
+    last_update timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_name, show_id),
+    CONSTRAINT `fk_users_shows_user` FOREIGN KEY (`user_name`) REFERENCES `users` (`user_name`) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT `fk_users_shows_shows` FOREIGN KEY (`show_id`) REFERENCES `shows` (`show_id`) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
-
-#not necesaary,just an option for loading all the data in one big data without normalization etc.
-#CREATE TABLE netflix_stage (
-#  `show_id` bigint DEFAULT NULL,
-#  `type` text,
-#  `title` text,
-#  `director` text,
-#  `cast` text,
-#  `country` text,
-#  `date_added` datetime DEFAULT NULL,
-#  `release_year` bigint DEFAULT NULL,
-# `rating` text,
-#  `duration` text,
-#  `listed_in` text,
-#  `description` text,
-#  `season_count` text
-#) ;
+#not necesaary,just an option for loading all the data in one big table without normalization etc.
+CREATE TABLE netflix_stage (
+  `show_id` bigint DEFAULT NULL,
+  `type` text,
+  `title` text,
+  `director` text,
+  `cast` text,
+  `country` text,
+  `date_added` datetime DEFAULT NULL,
+  `release_year` bigint DEFAULT NULL,
+ `rating` text,
+  `duration` text,
+  `listed_in` text,
+  `description` text,
+  `season_count` text
+) ;
